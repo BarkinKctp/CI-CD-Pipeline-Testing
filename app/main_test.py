daf212a665afc79aef4df66a900985b0a9ece493
BarkinKctp <barkinkocatepe12@gmail.com>
2026-02-25 19:15:16 +0300

    Initialize Repository

---
+++ app/main_test.py
@@ -0,0 +1,15 @@
+from app.main import return_backwards_string, get_mode
+import unittest
+import os
+
+class TestMain(unittest.TestCase):
+    def test_return_backwards_string(self):
+        random_string = "Hello, World!"
+        random_string_backwards = "!dlroW ,olleH"
+        self.assertEqual(return_backwards_string
+                         (random_string), random_string_backwards)
+    def test_get_env(self):
+        self.assertEqual(os.environ.get("MODE", get_mode()), "No mode set")
+
+if __name__ == '__main__':
+    unittest.main()
