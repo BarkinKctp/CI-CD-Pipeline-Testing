05a7811a9b7c1bd9b6a3125c9654b335533d2ff9
BarkinKctp <barkinkocatepe12@gmail.com>
2026-02-27 00:18:34 +0300

    Add Flask app with basic routing and HTML templates for string reversal and mode display

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.', static_folder='.', static_url_path='/static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/<random_string>')
 def return_backwards_string(random_string):
+    reversed_text = "".join(reversed(random_string))
+    return render_template('reverse.html', input_text=random_string, reversed_text=reversed_text)
+
+@app.route('/api/reverse/<random_string>')
+def reverse_string_api(random_string):
     return "".join(reversed(random_string))
 
 @app.route('/get-mode')
 def get_mode():
-    return os.environ.get("MODE", "No mode set")
+    mode_value = os.environ.get("MODE", "No mode set")
+    return render_template('mode.html', mode_value=mode_value)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
